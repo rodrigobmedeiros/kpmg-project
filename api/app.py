5cdfd7cb80134a7b4f44f1710f8d1678836f7764
rodrigo <rodrimedeiros@gmail.com>
2021-04-25 19:24:08 -0300

    Implement endpoint to get mean value for a specific manufacturer

--- api/app.py
+++ api/app.py
@@ -8,6 +8,7 @@ def create_app(test_config=None):
     Function used to define all end points and error handling.
     """
     app = Flask(__name__)
+    app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
     data_info = datasetGenerator('config.json')
 
@@ -16,6 +17,19 @@ def create_app(test_config=None):
     @app.route('/mean-values-per-manufacturer', methods=['GET'])
     def get_mean_values_per_manufacturer():
 
-        return jsonify({'sucess': True, 'manufacturer_mean_values': grouped_data.formatted_data})
+        return jsonify({'sucess': True,
+                        'manufacturer_mean_values': grouped_data.formatted_data})
+
+
+    @app.route('/mean-values-per-manufacturer/<manufacturer>', methods=['GET'])
+    def get_mean_value_by_manufacturer(manufacturer):
+
+        manufacturer_mean_value = grouped_data.data_agg_by_mean_value.loc[manufacturer].values[0]        
+
+        return jsonify({'sucess': True,
+                        'manufacturer': manufacturer,
+                        'mean_value': round(manufacturer_mean_value, 2)})
+
+
 
     return app
