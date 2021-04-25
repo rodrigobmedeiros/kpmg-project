e29ef837fb9d0cb39d14a5f636818e4cadbb952d
rodrigo <rodrimedeiros@gmail.com>
2021-04-25 14:57:59 -0300

    Include test of read config file class

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,8 +1,17 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join('..')))
 from flask import Flask
+from api.modules.datasetGenerator import datasetGenerator
 
 def create_app(test_config=None):
     """
     Function used to define all end points and error handling.
     """
+    app = Flask(__name__)
+    df = datasetGenerator()
+    print(df.columns)
+
+
+print(os.path.dirname(os.path.realpath(__file__)))
 
     
