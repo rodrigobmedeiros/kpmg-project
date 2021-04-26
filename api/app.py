2d503cac06a3fe6b192e393e3c1f095b800c8f92
rodrigo <rodrimedeiros@gmail.com>
2021-04-25 22:47:37 -0300

    Implement endpoints based on spark data treatment

--- api/app.py
+++ api/app.py
@@ -10,9 +10,15 @@ def create_app(test_config=None):
     app = Flask(__name__)
     app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
+    # Data treatment using pandas
     data_info = datasetGenerator('config.json')
 
     grouped_data = dataGrouper(data_info.dataframe)
+
+    # Data treatment using spark
+    data_info_spark = datasetGeneratorSpark('config.json')
+
+    grouped_data_by_city = dataGrouperSpark(data_info_spark.dataframe)
 
     @app.route('/mean-values-per-manufacturer', methods=['GET'])
     def get_mean_values_per_manufacturer():
@@ -30,6 +36,22 @@ def create_app(test_config=None):
                         'manufacturer': manufacturer,
                         'mean_value': round(manufacturer_mean_value, 2)})
 
+    @app.route('/mean-values-per-cities', methods=['GET'])
+    def get_mean_values_per_cities():
 
+        return jsonify({'sucess': True,
+                        'manufacturer_mean_values': grouped_data_by_city.formatted_data})
+
+    @app.route('/mean-values-per-cities/<city>', methods=['GET'])
+    def get_mean_value_by_manufacturer(city):
+
+        # default column name created when mean was applied.
+        avg_column = 'avg(car_value)'
+        filtered_city = grouped_data_by_city.data_agg_by_mean_value['city' == city]
+        city_mean_value = filtered_city[avg_column]      
+
+        return jsonify({'sucess': True,
+                        'manufacturer': city,
+                        'mean_value': round(city_mean_value, 2)})
 
     return app
