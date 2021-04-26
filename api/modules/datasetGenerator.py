f897edef6e66d264fc21a3f97a004148c156deff
rodrigo <rodrimedeiros@gmail.com>
2021-04-25 22:24:00 -0300

    Implement new class to treat data using spark

--- api/modules/datasetGenerator.py
+++ api/modules/datasetGenerator.py
@@ -1,9 +1,13 @@
 import json
 import pandas as pd
+from pyspark.sql import SQLContext
+from pyspark import SparkContext
+from pyspark.sql.functions import col, column, round as roundp
 
 class datasetGenerator(object):
     """
     Class responsible for parse a config.json file into a avaiable dataset.
+    Data treatment using pandas.
     """
     def __init__(self, json_path='config.json'):
         
@@ -22,7 +26,10 @@ class datasetGenerator(object):
     def _create_dataframe(self):
         
         complete_filename = ''.join(['src/', self._data_filename])
-        dataframe = pd.read_csv(complete_filename)
+        sc = SparkContext()
+        sqlContext = SQLContext(sc)
+        dataframe = sqlContext.read.option("header", True).csv(complete_filename)
+        dataframe = dataframe.withColumn("car_value", col("car_value").cast("double"))
         return dataframe
 
     @property
@@ -30,4 +37,6 @@ class datasetGenerator(object):
 
         return self._dataframe
 
-datasetGenerator()
+class datasetGeneratorSpark(datasetGenerator):
+
+
